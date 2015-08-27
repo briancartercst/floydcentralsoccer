0d1ea2d6971630b9811f9831f9d55434cb5837e3
Brian Carter <briancarter@chipsofttech.com>
2015-08-27 00:20:44 -0400

    try removing check for team id

--- site/js/site.js
+++ site/js/site.js
@@ -2,10 +2,13 @@
 //teamnode init is fired first
 
 $( document ).ready(function() {
+	
+	/*
 	if(window.location.pathname != "/" && window.location.pathname != "/dashboard" && getQueryVariable("teamid") == null)
 	{
 		window.location = window.location.protocol + "//" + window.location.host;
-	}	
+	}
+    */	
 	
 	$('.header').html(header());
 	$('.footer').html(footer());
