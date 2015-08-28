ba025d8be52ad9fbc9891ab333ad0b3eab2d858a
Brian Carter <briancarter@chipsofttech.com>
2015-08-28 07:53:22 -0400

    add data

--- site/js/site.js
+++ site/js/site.js
@@ -161,8 +161,8 @@ function headElement() {
 	'<meta name="viewport" content="width=device-width, initial-scale=1">' +
 	'<link rel="stylesheet" href="./css/bootstrap.min.css">' +
 	'<link href="./css/site.css" rel="stylesheet">' +
-	'<link href="./css/bootstrap-responsive.css" rel="stylesheet">' +
-	'<link rel="stylesheet" href="./font-awesome/css/font-awesome.css">' +	
+	'<link href="./css/bootstrap-responsive.min.css" rel="stylesheet">' +
+	'<link rel="stylesheet" href="./font-awesome/css/font-awesome.min.css">' +	
 	'<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->' +
 	'<!--[if lt IE 9]>' +
 	'<script src="./js/html5shiv.js"></script>' +
